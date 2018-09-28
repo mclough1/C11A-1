5119b16f7421461b779777393fd8832b327d0e3a
Matthew Clough <mclough@mymail.mines.edu>
2018-09-28 15:38:14 -0600

    Changed font font size and color to blue

--- src/Cat.java
+++ src/Cat.java
@@ -41,9 +41,10 @@ public class Cat {
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
-		g2.setColor(Color.black);
+		g2.setColor(Color.blue);
+		g2.setFont(new Font("Helvetica", Font.PLAIN, 26));
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Rawr", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
